e781bddd9b0f439e0190cee331e2edf2a793c9a9
Shreshth Srivastava <shreshth28srivastava@gmail.com>
2025-11-21 19:01:26 -0700

    add ccommand parsing with PING and ECHO

--- main.cpp
+++ main.cpp
@@ -1,5 +1,53 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
+
+
+std::vector<std::string> split_line(const std::string &line)
+{
+    std::vector<std::string> tokens;
+    std::istringstream iss(line);
+    std::string token;
+
+    while(iss>>token){
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+bool handle_command(const std::vector<std::string> &tokens){
+    if(tokens.empty())
+        return true;
+    const std::string &cmd=tokens[0];
+    if(cmd=="exit"|| cmd=="quit")
+    {
+        std::cout<<"Goodbye!"<<std::endl;
+        return false;
+    }
+    if(cmd=="PING" || cmd=="ping")
+    {
+        std::cout<<"PONG"<<std::endl;
+        return true;
+    }
+    if(cmd=="ECHO" || cmd=="echo")
+    {
+        if(tokens.size()<=1)
+            std::cout<<"(nil)"<<std::endl;
+        else{
+            for(std::size_t i=1;i<tokens.size();i++){
+                if(i>1)
+                    std::cout<<' ';
+            std::cout<<tokens[i];
+            }
+        std::cout<<std::endl;
+            }
+        return true;
+        
+    }
+    std::cout<<"ERR unknown command: "<< cmd<<std::endl;   
+    return true;
+}
 
 int main()
 {
@@ -13,12 +61,10 @@ int main()
             std::cout<<"\nInpput closed, shutting down MiniRedis."<<std::endl;
             break;
         }
-
-        if(line=="exit"||line=="quit"){
-            std::cout<<"Goodbye!"<<std::endl;
+        std::vector<std::string> tokens=split_line(line);
+        bool keep_running=handle_command(tokens);
+        if(!keep_running)
             break;
-        }
-        std::cout<<"You said: "<<line<<std::endl;
     }
     return 0;
 }
